05b713ecf9d04a8077c37fe9c011a293b3e8c1f9
kobebryant24 <bryanmanncunha@gmail.com>
2019-11-12 23:29:21 -0300

    alterações

--- lib/telas/paineltutuorado.dart
+++ lib/telas/paineltutuorado.dart
@@ -11,8 +11,41 @@ class _PainelTutoradoState extends State<PainelTutorado> {
       appBar: AppBar(
         title: Text("painel tutorado"),
       ),
-      body: Container(),
-
-    );
+      body: Column(
+      
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Row(
+        children: <Widget>[ Container(child: ConstrainedBox(
+    constraints: BoxConstraints.expand(),
+    child: FlatButton(onPressed: null,
+        child: Image.asset('assets/botao_alerta.png'))
+        )),
+         Container(child: ConstrainedBox(
+    constraints: BoxConstraints.expand(),
+    child: FlatButton(onPressed: null,
+        child: Image.asset('assets/rota.png'))
+        ))],
+        
+        
+          ),
+          Row(
+        children: <Widget>[ Container(child: ConstrainedBox(
+    constraints: BoxConstraints.expand(),
+    child: FlatButton(onPressed: null,
+        child: Image.asset('assets/mapa.png'))
+        )),
+         Container(child: ConstrainedBox(
+    constraints: BoxConstraints.expand(),
+    child: FlatButton(onPressed: null,
+        child: Image.asset('assets/chat.png'))
+        ))],
+        
+        
+          ),
+        ],
+      )
+      );
+    
   }
 }
